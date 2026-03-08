e71ef2ef4af098c1866e7f695d3fec1bb40d3368
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-09 00:55:09 +0800

    🐛修复：编译超时

--- src/main/java/com/liuyue/igny/mixins/rule/disableWatchDog/ServerWatchdogMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/disableWatchDog/ServerWatchdogMixin.java
@@ -3,23 +3,22 @@ package com.liuyue.igny.mixins.rule.disableWatchDog;
 import com.liuyue.igny.IGNYSettings;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraft.server.dedicated.ServerWatchdog;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Environment(EnvType.SERVER)
-@Mixin(value = ServerWatchdog.class)
+@Mixin(ServerWatchdog.class)
+@Pseudo
 public class ServerWatchdogMixin {
     @Shadow
     @Final
     private DedicatedServer server;
 
-    @WrapOperation(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/Util;getNanos()J"))
+    @WrapOperation(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/Util;getNanos()J"), require = 0)
     private long getNanos(Operation<Long> original) {
         if (IGNYSettings.disableWatchDog) return this.server.getNextTickTime();
         return original.call();
