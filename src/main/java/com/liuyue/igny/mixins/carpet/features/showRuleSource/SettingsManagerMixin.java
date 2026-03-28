5a02fcffe8ae8dbf234ca51daa6796457376bfd5
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-28 16:21:17 +0800

    🐛修复：一些地毯拓展无法被显示规则来源 (showRuleSource)识别的问题

--- src/main/java/com/liuyue/igny/mixins/carpet/features/showRuleSource/SettingsManagerMixin.java
+++ src/main/java/com/liuyue/igny/mixins/carpet/features/showRuleSource/SettingsManagerMixin.java
@@ -1,27 +1,25 @@
 package com.liuyue.igny.mixins.carpet.features.showRuleSource;
 
+import carpet.api.settings.CarpetRule;
 import carpet.api.settings.SettingsManager;
 import com.liuyue.igny.IGNYSettings;
 import com.liuyue.igny.utils.ClassUtil;
+import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 @Mixin(SettingsManager.class)
 public class SettingsManagerMixin {
-    @Inject(method = "parseSettingsClass", at = @At(value = "TAIL"))
-    private static void parseSettingsClass(Class<?> settingsClass, CallbackInfo ci) {
+    @Inject(method = "parseSettingsClass", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
+    private static void parseSettingsClass(Class<?> settingsClass, CallbackInfo ci, @Local(name = "parsed") CarpetRule<?> rule) {
         ClassUtil.getModIdFromClass(settingsClass, modId ->
-                Arrays.stream(settingsClass.getDeclaredFields()).forEach(field -> {
-                    String ruleName = field.getName();
-                    IGNYSettings.modRuleTree
-                            .computeIfAbsent(modId, k -> new ArrayList<>())
-                            .add(ruleName);
-                })
+                IGNYSettings.modRuleTree
+                .computeIfAbsent(modId, k -> new ArrayList<>())
+                .add(rule.name())
         );
     }
 }
